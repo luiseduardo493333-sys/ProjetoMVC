ef4cd082b7066a1e4c22c65d62a83eef92ad1ac9
Luis <Luiseduardo493333@gmail.com>
2026-05-05 16:35:47 -0300

    Funções token feita

---
+++ app/auth.py
@@ -0,0 +1,45 @@
+# 1. Hash e verificação de senha com bcrpt
+#2. Gerção de token JWT
+# 3. leitrua e validção do token vindo de cookie
+
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import requests, HTTPException, status
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALOGRITHM = os.getenv("ALGORITHM")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACESS_TOKEN_EXPIRE_MINUTES")
+
+#CryptContext - configure o bycrpt como algoritmo de hash
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# funções de senha
+def hash_senha(senha: str):
+    return pwd_context.hash(senha)
+
+def verificar_senha(senha: str, senha_hash: str):
+    return pwd_context.verify(senha = senha_hash)
+
+#  funções do token - JWT
+def criar_toke(data: dict):
+    payload = data.copy()
+
+    # definir quando o token vai expirar
+    expira = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload.update({"exp": expira})
+
+    # Criar o token - JWT
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALOGRITHM)
+    return token
+
+def decodificar_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALOGRITHM])
+    return payload
